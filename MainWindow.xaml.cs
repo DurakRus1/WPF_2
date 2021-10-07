c5966acfb9eaf1f40ea909be07afb549d884234f
ilya <marfin296@gmail.com>
2021-10-07 20:39:07 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Ink;
+
+namespace Var4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawButton_Checked(object sender, RoutedEventArgs e)
+        {
+            this.DrawingCanvas.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+        private void EraseButton_Checked(object sender, RoutedEventArgs e)
+        {
+            this.DrawingCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+    }
+}
